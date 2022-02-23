eed1c6d22426adac5f8a62e15d1a969d046c2645
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-02-23 23:19:58 +0300

    create test working fine

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -2,30 +2,33 @@ const db = require("../models");
 const Data = db.data;
 // Create and Save a new Data
 exports.create = (req, res) => {
-    res.send('created')
-    // // Validate request
-    // if (!req.body.data) {
-    //   res.status(400).send({ message: "Content can not be empty!" });
-    //   return;
-    // }
-    // // Create a Data
-    // const data = new Data({
-    //   data: req.body.data,
-    //   accessTimesCount: req.body.accessTimesCount,
-    //   expirationTime: req.body.expirationTime
-    // });
-    // // Save Tutorial in the database
-    // data
-    //   .save(data)
-    //   .then(data => {
-    //     res.send(data);
-    //   })
-    //   .catch(err => {
-    //     res.status(500).send({
-    //       message:
-    //         err.message || "Some error occurred while creating the Data."
-    //     });
-    //   });
+
+    console.log(req.body.data)
+    console.log(req.body.accessTimesCount)
+    console.log(req.body.expirationTime)
+    //Validate request
+    if (!req.body.data) {
+      res.status(400).send({ message: "Content can not be empty!" });
+      return;
+    }
+    // Create a Data
+    const data = new Data({
+      data: req.body.data,
+      accessTimesCount: req.body.accessTimesCount,
+      expirationTime: req.body.expirationTime
+    });
+    // Save Data in the database
+    data
+      .save(data)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Data."
+        });
+      });
   };
 
 
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 require("./routes/data.routes")(app);
 
