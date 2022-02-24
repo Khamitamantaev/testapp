f553b04ae4c89f8fc2192598866231edae99e4d7
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-02-24 15:00:50 +0300

    add moment with node-cron to server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 var bodyParser = require('body-parser')
+var moment = require('moment');
 const app = express()
 const port = 3000
-
+const cron = require("node-cron");
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -12,6 +13,7 @@ app.use(bodyParser.json())
 require("./routes/data.routes")(app);
 
 const db = require("./models");
+const Data = db.data
 
 db.mongoose
   .connect(db.url, {
@@ -25,6 +27,17 @@ db.mongoose
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
+
+//cron with Date Check
+cron.schedule("* * * * *", function () {
+  var now = moment().toDate();
+  console.log(now)
+  Data.deleteMany({ expirationTime: { $lte: now } }, function (err) {
+    if (err) {
+      console.log(err)
+    }
+  })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
