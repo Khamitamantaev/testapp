47645ec92e9b2d497238f07bf9733e9e0a8a6faa
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-02-24 11:29:48 +0300

    change error send

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -83,7 +83,7 @@ exports.update = (req, res) => {
   })
   .catch(err => {
     res.status(500).send({
-      message: "Error updating Tutorial with id=" + id
+      message: "Error updating Data with adminCode =" + adminCode
     });
   });
 };
